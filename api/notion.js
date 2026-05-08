ae8b21217307eb2c2a06e111e2e5e16bea284cf7
Luzz2z <trix.hop@gmail.com>
2026-05-08 14:51:18 +0200

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -1,47 +1,40 @@
-export const config = {
-api: {
-bodyParser: true,
-},
-};
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
 res.setHeader(‘Access-Control-Allow-Origin’, ‘*’);
 res.setHeader(‘Access-Control-Allow-Methods’, ‘GET, POST, PATCH, OPTIONS’);
 res.setHeader(‘Access-Control-Allow-Headers’, ‘Content-Type’);
 
-if (req.method === ‘OPTIONS’) return res.status(200).end();
-
-const endpoint = req.query.endpoint;
-if (!endpoint) return res.status(400).json({ error: ‘Missing endpoint’ });
-
-const notionUrl = ‘https://api.notion.com/v1/’ + endpoint;
-
-let bodyString = undefined;
-if (req.method === ‘POST’ || req.method === ‘PATCH’) {
-try {
-bodyString = typeof req.body === ‘string’ ? req.body : JSON.stringify(req.body);
-} catch(e) {
-bodyString = ‘{}’;
-}
+if (req.method === ‘OPTIONS’) {
+res.status(200).end();
+return;
 }
 
-try {
-const response = await fetch(notionUrl, {
+var endpoint = req.query.endpoint;
+if (!endpoint) {
+res.status(400).json({ error: ‘Missing endpoint’ });
+return;
+}
+
+var notionUrl = ‘https://api.notion.com/v1/’ + endpoint;
+var token = process.env.NOTION_TOKEN;
+
+var options = {
 method: req.method,
 headers: {
-‘Authorization’: ’Bearer ’ + process.env.NOTION_TOKEN,
+‘Authorization’: ’Bearer ’ + token,
 ‘Notion-Version’: ‘2022-06-28’,
 ‘Content-Type’: ‘application/json’,
 },
-body: bodyString,
-});
+};
 
-```
-const data = await response.json();
-return res.status(response.status).json(data);
-```
+if (req.method === ‘POST’ || req.method === ‘PATCH’) {
+options.body = JSON.stringify(req.body || {});
+}
 
+try {
+var response = await fetch(notionUrl, options);
+var data = await response.json();
+res.status(response.status).json(data);
 } catch (err) {
-return res.status(500).json({ error: err.message });
+res.status(500).json({ error: err.message || ‘Unknown error’ });
 }
-}
+};
