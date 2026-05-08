49a95e26651b7f2fb6ef39dab8e66aed5e6a8614
Luzz2z <trix.hop@gmail.com>
2026-05-08 18:44:17 +0200

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -7,11 +7,27 @@ module.exports = async function handler(req, res) {
 
   var fullUrl = req.url || '';
   var endpoint = fullUrl.replace(/^\/api\/notion\/?/, '').split('?')[0];
-  var notionUrl = 'https://api.notion.com/v1/' + endpoint;
+  if (!endpoint) { res.status(400).json({ error: 'Missing endpoint' }); return; }
 
-  res.status(200).json({ 
-    endpoint: endpoint,
-    notionUrl: notionUrl,
-    method: req.method
-  });
+  var notionUrl = 'https://api.notion.com/v1/' + endpoint;
+  var options = {
+    method: req.method,
+    headers: {
+      'Authorization': 'Bearer ' + process.env.NOTION_TOKEN,
+      'Notion-Version': '2022-06-28',
+      'Content-Type': 'application/json',
+    },
+  };
+
+  if (req.method === 'POST' || req.method === 'PATCH') {
+    options.body = JSON.stringify(req.body || {});
+  }
+
+  try {
+    var response = await fetch(notionUrl, options);
+    var data = await response.json();
+    res.status(response.status).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
